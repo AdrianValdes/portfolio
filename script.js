078d3f9e2ce29259fe969ce11f7ecdc934da5bde
AdrianValdes <adrianvaldeshdz@.gmail.com>
2020-10-01 08:23:39 +0200

    Changing the initial icon to an arrow

--- script.js
+++ script.js
@@ -3,17 +3,43 @@ const fakeHeight = document.getElementById('fake-height');
 const arts = document.getElementsByClassName('art');
 const glitch = document.getElementById('glitch');
 const arrow = document.getElementById('arrow');
+const innerWidth = window.innerWidth;
 
 window.onload = () => {
-  const scrollHeight = scrollContent.getBoundingClientRect().width;
-  fakeHeight.style.height = `${scrollHeight}px`;
+  const scrollWidth = scrollContent.getBoundingClientRect().width;
+  const scrollHeight = scrollContent.getBoundingClientRect().height;
+
+  console.log('scrollWidth :' + scrollWidth);
+
+  console.log('scrollHeight : ' + scrollHeight);
+
+  if (scrollWidth / scrollHeight > 8) {
+    fakeHeight.style.height = `${scrollWidth + scrollHeight}px`;
+  }
+
+  if (scrollWidth / scrollHeight > 0.5 && scrollWidth / scrollHeight < 2) {
+    fakeHeight.style.height = `${scrollWidth}px`;
+  }
+  if (scrollWidth / scrollHeight > 2 && scrollWidth / scrollHeight < 5) {
+    fakeHeight.style.height = `${scrollWidth + scrollHeight - 100}px`;
+    console.log('hello');
+  }
+  if (scrollWidth / scrollHeight >= 5 && scrollWidth / scrollHeight <= 8) {
+    fakeHeight.style.height = `${scrollWidth - 300}px`;
+  }
 
   window.addEventListener('scroll', (e) => {
-    translate(window.pageYOffset);
+    if (window.pageYOffset < 7400) {
+      translate(window.pageYOffset);
+      console.log('translated pixels: ' + window.pageYOffset);
+    }
   });
 
   function translate(pixels) {
     scrollContent.style.transform = `translateX(${-pixels}px)`;
+    if (pixels > 50) {
+      arrow.style.transform = `rotate(${0}deg) `;
+    }
   }
 };
 
@@ -28,3 +54,7 @@ function toggleGlitch() {
     glitch.classList.replace('no-glitch', 'glitch');
   }
 }
+
+arrow.addEventListener('click', () => {
+  arrow.style.transform = `rotate(${90}deg) `;
+});
